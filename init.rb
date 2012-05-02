875cb74459a16212c74bf090a8e60170462418b0
Troy Davis <troy@sevenscale.com>
2012-05-01 18:05:38 -0700

    Show error when papertrail gem is not installed

--- init.rb
+++ init.rb
@@ -1,4 +1,8 @@
-require File.dirname(__FILE__) + '/lib/papertrail'
+begin
+  require File.dirname(__FILE__) + '/lib/papertrail'
+rescue LoadError
+  raise "papertrail gem is missing. Please install papertrail: gem install papertrail"
+end
 
 Heroku::Command::Help.group('Papertrail') do |group|
   group.command 'pt:logs [-t] [query]', 'Show Papertrail logs (optional: tail; filter with query)'
