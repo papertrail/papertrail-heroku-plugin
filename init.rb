5dacc9b161e5f281c58f66d6f2b14cc046e98815
Troy Davis <troy@sevenscale.com>
2012-05-01 17:11:24 -0700

    Remove .rb from require statement

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/lib/papertrail.rb'
+require File.dirname(__FILE__) + '/lib/papertrail'
 
 Heroku::Command::Help.group('Papertrail') do |group|
   group.command 'pt:logs [-t] [query]', 'Show Papertrail logs (optional: tail; filter with query)'
