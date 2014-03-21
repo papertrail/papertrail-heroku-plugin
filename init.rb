8b328e6d07041baf7768020c3caaf5611bec6f80
Troy Davis <troy@yort.com>
2014-03-21 09:57:47 -0700

    Remove 'begin' no-op

--- init.rb
+++ init.rb
@@ -1,6 +1,4 @@
-begin
-  require File.dirname(__FILE__) + '/lib/papertrail'
-end
+require File.dirname(__FILE__) + '/lib/papertrail'
 
 Heroku::Command::Help.group('Papertrail') do |group|
   group.command 'pt:logs [-t] [query]', 'Search and/or tail logs (optional: tail; search query)'
