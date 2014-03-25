52baec3851156d755e11a22d0637acecdd2e4f02
Troy Davis <troy@yort.com>
2014-03-25 09:24:52 -0700

    Require path

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-require 'vendor/papertrail'
+require 'papertrail-heroku-plugin/vendor/papertrail'
 
 Heroku::Command::Help.group('Papertrail') do |group|
   group.command 'pt:logs [-t] [query]', 'Search and/or tail logs (optional: tail; search query)'
