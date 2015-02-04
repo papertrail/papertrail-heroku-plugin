a9a80851f76e551162bff7c571572ada1ae50604
Larry Marburger <larry@marburger.cc>
2015-02-04 14:22:18 -0500

    Add tail option

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var Heroku = require('heroku-client');
-var https = require('https');
-var strftime = require('strftime');
+var Heroku = require("heroku-client");
+var https = require("https");
+var qs = require("querystring");
+var strftime = require("strftime");
 
 exports.topics = [{
   name: "pt2",
@@ -17,44 +18,74 @@ exports.commands = [{
 
   needsApp: true,
   needsAuth: true,
+  flags: [{name: "tail", char: 0x0074, hasValue: false}],
+  args: [{name: "query", optional: true}],
   run: function (context) {
-    var heroku = new Heroku({token: context.auth.password});
-    heroku.apps(context.app).configVars().info(function (err, config) {
-      if (err) { throw err; }
-      if (!config.PAPERTRAIL_API_TOKEN) {
-        console.error('Add the Papertrail add-on to this application: https://addons.heroku.com/papertrail');
-        process.exit(1);
-      }
+    new Heroku({token: context.auth.password})
+      .apps(context.app)
+      .configVars()
+      .info(function (err, config) {
+        if (err) { throw err; }
+        if (!config.PAPERTRAIL_API_TOKEN) {
+          console.error("Add the Papertrail add-on to this application: https://addons.heroku.com/papertrail");
+          process.exit(1);
+        }
 
-      var options = {
-        host: 'papertrailapp.com',
-        path: '/api/v1/events/search.json',
-        port: '443',
-        headers: {'X-Papertrail-Token': config.PAPERTRAIL_API_TOKEN}
-      };
-
-      var printEvent = function(event) {
-        var received_at = strftime("%b %e %T", new Date(event.received_at));
-        console.log(received_at + " " + event.hostname + " " + event.program + ": " + event.message);
-      };
-
-      var process = function(data) {
-        JSON.parse(data).events.forEach(printEvent);
-      };
-
-      callback = function(response) {
-        var data = "";
-
-        response.on('data', function (chunk) {
-          data += chunk;
-        });
-
-        response.on('end', function () {
-          process(data);
-        });
-      }
-
-      https.get(options, callback);
-    });
+        search(config.PAPERTRAIL_API_TOKEN, context.args.query, context.args.tail);
+      });
   }
 }];
+
+
+var tailDelay = 1000;
+
+var printEvent = function(event) {
+  var received_at = strftime("%b %e %T", new Date(event.received_at));
+  console.log(received_at + " " + event.hostname + " " + event.program + ": " + event.message);
+};
+
+var process = function(data) {
+  var parsedData = JSON.parse(data);
+  parsedData.events.forEach(printEvent);
+  return parsedData.max_id;
+};
+
+var search = function(token, query, tail, minId) {
+  path = "/api/v1/events/search.json";
+  if (query || minId) {
+    path += "?";
+    if (query) { path += qs.stringify({q: query}); }
+    if (path && query) { path += "&"; }
+    if (query) { path += qs.stringify({min_id: minId}); }
+  }
+
+  var options = {
+    host: "papertrailapp.com",
+    path: path,
+    port: 443,
+    headers: {"X-Papertrail-Token": token}
+  };
+
+  var maxId;
+  callback = function(response) {
+    var data = "";
+
+    response.on("data", function (chunk) {
+      data += chunk;
+    });
+
+    response.on("end", function () {
+      maxId = process(data);
+
+      if (tail) {
+        setTimeout(function() { search(token, query, tail, maxId) }, tailDelay);
+      }
+    });
+  }
+
+  https
+    .get(options, callback)
+    .on('error', function(e) {
+      console.error("ERROR:" + e);
+    });
+};
